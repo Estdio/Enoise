0c491b7f469eb170971462d416132117d6accdb4
smokeytube <zachary.a.davis.6@gmail.com>
2021-05-10 21:25:33 -0400

    Added files

---
+++ enoise/android/app/src/main/kotlin/dev/estdio/enoise/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.estdio.enoise
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ enoise/lib/main.dart
@@ -0,0 +1,126 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:requests/requests.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Enoise',
+      home: MyHomePage(title: 'Enoise'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _httpcontent = '';
+  bool _booltoggle = false;
+  var _urllist = [
+    "https://wedoaliltrollin.com/",
+    "https://twitter.com/",
+    "https://4chan.org",
+    "https://www.reddit.com",
+    "https://www.yahoo.com",
+    "https://www.cnn.com",
+    "https://www.ebay.com",
+    "https://wikipedia.org",
+    "https://youtube.com",
+    "https://github.com/smokeytube",
+    "https://medium.com",
+    "https://flutter.dev/",
+    "https://forum.hackthebox.eu/profile/xeno503",
+    "https://github.com/Estdio",
+    "https://www.youtube.com/",
+    "https://www.roblox.com/"
+  ];
+
+  void makerequest() async {
+    final _random = new Random();
+    String requrl = _urllist[_random.nextInt(_urllist.length)];
+    try {
+      var r = await Requests.get(requrl);
+      r.raiseForStatus();
+      _httpcontent = "[200] " + requrl;
+      debugPrint(_httpcontent);
+    } catch (TimeoutException) {
+      _httpcontent = "[408] TimeoutException: " + requrl;
+    }
+  }
+
+  void requesttoggle() {
+    debugPrint('here');
+    if (_booltoggle) {
+      _booltoggle = false;
+    } else {
+      _booltoggle = true;
+    }
+  }
+
+  void updatescreen() async {
+    _booltoggle = true;
+    while (_booltoggle) {
+      setState(() {
+        makerequest();
+      });
+      //sleep(Duration(seconds: 1));
+      debugPrint('before:delay');
+      await Future.delayed(Duration(seconds: 1));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: Scaffold(
+            appBar: AppBar(title: Text('Enoise')),
+            body: Center(
+              child: Container(
+                constraints: BoxConstraints.expand(),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/E.png"),
+                        fit: BoxFit.scaleDown)),
+                child: Center(
+                  child: Text(
+                    _httpcontent,
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+              ),
+            ),
+            floatingActionButton: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: updatescreen,
+                    child: Icon(Icons.network_cell),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: requesttoggle,
+                    child: Icon(Icons.stop_circle_outlined),
+                  ),
+                ),
+              ],
+            )));
+  }
+}
